e2f37b992351eba9b00a75298871d59fb2f45d1b
DESKTOP-T2NFE3P\yeon <wndusrj98@naver.com>
2025-09-18 17:14:14 +0900

    0918commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $("#gnb .dep1>li").hover(
   function () {
     $(this).children(".dep2_wrap").stop().show();
+    $("header").addClass("on");
   },
   function () {
     $("#gnb .dep2_wrap").stop().slideUp();
+    $("header").removeClass("on");
   }
 );
